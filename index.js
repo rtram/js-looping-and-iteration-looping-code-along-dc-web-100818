514e33c8f1201c1e3ff429e0289c45a73bb07aac
Robin Tram <robin.tram@gmail.com>
2018-11-18 10:54:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function printBadges(arr) {
+  for (i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+  }
+  return arr;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let i = 0
+  while (maybeTrue) {
+    i++
+  }
+  return `You got ${i} tails in a row!`
+}
